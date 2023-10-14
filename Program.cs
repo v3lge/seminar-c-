b36443895b38279558bd59d90c7ff1690353911e
Gleb <dalton.sot@mail.ru>
2023-10-14 14:36:35 +0700

    the lesson is over

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿// System.Console.Write("input ur age: ");
+// int age = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"Ur age ->  {age} !!! ");
+
+
+// System.Console.WriteLine("Input the number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine($"The square is -> {number * number}!!!");
+
+// System.Console.WriteLine("input the number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number > 99 && number <1000)
+// {
+//     System.Console.WriteLine($"The last digit is {number % 10} !!!");
+// }
+// else
+// {
+//     System.Console.WriteLine("Invalid input");
+// }
+
+// System.Console.WriteLine("input the number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int current = -1* Math.Abs(n);
+
+// while (current <= Math.Abs(n))
+// {
+//     System.Console.WriteLine(current + " ");
+//     current++;
+// }
+
+
+// System.Console.WriteLine("input first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("input second number: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if ((b*b) == a)
+// {
+//     System.Console.WriteLine($"Congrats! The numbers are {a} and {b}!!");
+// }
+// else
+// {
+//      System.Console.WriteLine("Invalid input");
+// }
+
+// System.Console.WriteLine("input first number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// if (a == 1)
+// {
+//     System.Console.WriteLine("its monday");
+// }
+// else if (a==2)
+// {
+//     System.Console.WriteLine("its tuesday");
+// }
+// else if (a == 3)
+// {
+//     System.Console.WriteLine("its Wednesday");
+// }
+// else if (a==4)
+// {
+//     System.Console.WriteLine("its thursday");
+// }
+// else if (a==5)
+// {
+//     System.Console.WriteLine("its friday");
+// }
+// else if (a==6)
+// {
+//     System.Console.WriteLine("its saturday");
+// }
+// else if (a==7)
+// {
+//     System.Console.WriteLine("its sunday");
+// }
+// else
+// {
+//     System.Console.WriteLine("u r dumb");
+// }
